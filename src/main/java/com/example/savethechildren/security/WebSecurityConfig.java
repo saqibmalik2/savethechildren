d0acefa207693936436a115c71567a1a5c4ec8f6
saqibmalik2 <saqib.malik2@gmail.com>
2023-10-30 16:30:54 +0000

    Added Spring Security and disabled CORS and CSRF

---
+++ src/main/java/com/example/savethechildren/security/WebSecurityConfig.java
@@ -0,0 +1,28 @@
+package com.example.savethechildren.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+	
+	
+	@SuppressWarnings("removal")
+	@Bean
+	public SecurityFilterChain applicationSecurity(HttpSecurity http) throws Exception {
+		http.
+			cors().disable()
+			.csrf().disable()
+			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+			.formLogin().disable();
+		
+		return http.build();
+	}
+	
+
+}
