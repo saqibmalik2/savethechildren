1157d1eb7c93f8e08ed65f70056e06e87e0526be
saqibmalik2 <saqib.malik2@gmail.com>
2023-11-07 19:36:28 +0000

    Removed un-needed import

--- src/main/java/com/example/savethechildren/SavethechildrenApplication.java
+++ src/main/java/com/example/savethechildren/SavethechildrenApplication.java
@@ -2,7 +2,6 @@ package com.example.savethechildren;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 /**
  * @author Saqib Malik
